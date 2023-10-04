2db1474235e2bdd8336fd8409ee261e33e701c78
anktibose <abinfo2310@gmail.com>
2023-10-04 22:53:15 +0530

    Changes Made

---
+++ lib/Component/DetailPage.dart
@@ -0,0 +1,48 @@
+import 'package:api/Model/Blogs.dart';
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatelessWidget {
+  Blogs blog;
+  DetailPage({super.key, required this.blog});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Subspace', style: TextStyle(color: Colors.white),),
+        backgroundColor: Colors.black,
+        leading: IconButton(onPressed: (){
+          Navigator.of(context).pop();
+        }, icon: const Icon(Icons.arrow_back_ios)),
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(
+          vertical: 10.0,
+          horizontal: 10.0,
+        ),
+        child: Column(
+          children: [
+              Image.network(blog.image!),
+              const SizedBox(height: 10.0,),
+              Text(
+                blog.id!,
+                style: const TextStyle(
+                  fontFamily: "Verdana",
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 5.0,),
+              Text(
+                blog.title!,
+                style: const TextStyle(
+                  fontFamily: "Verdana",
+                  color: Colors.white,
+                ),
+              ),
+
+          ],
+        ),
+
+      ),
+    );
+  }
+}
--- lib/Component/homescreen.dart
+++ lib/Component/homescreen.dart
@@ -1,3 +1,4 @@
+import 'package:api/Component/DetailPage.dart';
 import 'package:api/Model/Blogs.dart';
 import 'package:api/Provider/BlogProvider.dart';
 import 'package:flutter/material.dart';
@@ -15,18 +16,17 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
-    
     Provider.of<BlogProvider>(context, listen: false).getAllBlogs();
-    
- 
   }
   
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Hello Welcome to ListView"),
+          backgroundColor: Colors.black,
+          title: const Text("Blogs and Articles", style: TextStyle(color: Colors.white),),
         ),
+        backgroundColor: const Color.fromARGB(221, 17, 17, 17),
         body:Consumer<BlogProvider>(
           builder: (context, value, child) {
             final blogs = value.blogs;
@@ -41,24 +41,31 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Widget buildCard(Blogs data) {
-  return Container(
-    padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-    decoration: const BoxDecoration(
-      color: Colors.black87, // Background color
-    ),
-    child: Column(
-      children: [
-        Image.network(
-          data.image.toString(),
-          fit: BoxFit.fill,
-        ),
-        Text(
-          data.title.toString(),
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
-        
-        const SizedBox(height: 10),
-      ],
+  return InkWell(
+    onTap: () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) {
+        return DetailPage(blog: data);
+      },));
+    },
+    child: Container(
+      padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+      decoration: const BoxDecoration(
+        color: Colors.black87, // Background color
+      ),
+      child: Column(
+        children: [
+          Image.network(
+            data.image.toString(),
+            fit: BoxFit.fill,
+          ),
+          Text(
+            data.title.toString(),
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          
+          const SizedBox(height: 10),
+        ],
+      ),
     ),
   );
 }
